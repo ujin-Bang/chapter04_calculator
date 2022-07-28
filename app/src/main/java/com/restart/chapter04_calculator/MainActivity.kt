666a8da013bb3bb189dc08b1379d2f2b22caee2f
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-07-28 17:49:04 +0900

    키페드 UI 완료

--- app/src/main/java/com/restart/chapter04_calculator/MainActivity.kt
+++ app/src/main/java/com/restart/chapter04_calculator/MainActivity.kt
@@ -2,10 +2,27 @@ package com.restart.chapter04_calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun buttonClicked(v: View){
+
+    }
+
+    fun resultButtonClicked(v: View){
+
+    }
+
+    fun historyButtonClicked(v: View){
+
+    }
+
+    fun clearButtonClicked(v: View){
+
     }
 }
